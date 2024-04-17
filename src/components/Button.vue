12ae6cb54cb165277f52d36cba89ecdca108846e
MCaipl <87945932+MCaipl@users.noreply.github.com>
2024-04-17 14:40:34 +0200

    Update Button.vue

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,21 +1,49 @@
 <script setup lang="ts">
-import {computed } from 'vue'
-const props = defineProps<{
-  label: string;
-  variant?: string
-}>();
+import { computed } from 'vue'
+  
+const props = withDefaults(defineProps<{
+  label?: string;
+  variant?: string;
+  ui?: unknown;
+}>(), {
+  variant: 'primary'
+});
+  
+const ui = {
+  wrapper: 'inline-flex items-center flex-shrink-0 py-1.5 px-2.5 gap-x-1.5 rounded-md font-medium text-sm cursor-pointer',
+  variants: {
+    primary: {
+      wrapper: 'bg-primary text-primary-content'
+    },
+    secondary: {
+      wrapper: 'bg-secondary text-secondary-content'
+    },
+    success: {
+      wrapper: 'bg-success text-success-content'
+    },
+    info: {
+      wrapper: 'bg-info text-info-content'
+    },
+    warning: {
+      wrapper: 'bg-warning text-warning-content'
+    },
+    danger: {
+      wrapper: 'bg-danger text-danger-content'
+    }
+  }
+}
 
-const colors = computed(() => props.variant === 'link' 
-  ? "text-emerald-500 hover:text-emerald-600 disabled:text-emerald-500 dark:text-emerald-400 dark:hover:text-emerald-500 dark:disabled:text-emerald-400 underline-offset-4 hover:underline focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-emerald-500 dark:focus-visible:ring-emerald-400 " 
-  : "text-white dark:text-gray-900 bg-emerald-500 hover:bg-emerald-600 disabled:bg-emerald-500 dark:bg-emerald-400 dark:hover:bg-emerald-500 dark:disabled:bg-emerald-400 focus-visible:outline-emerald-500 dark:focus-visible:outline-emerald-400"
-)
+
 </script>
 
 <template>
-  <div
-    class="cursor-pointer focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-1.5 px-2.5 py-1.5  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500 dark:focus-visible:outline-emerald-400 inline-flex items-center"
-    :class="[colors]"
-  >
-    {{ label }}
+  <div :class="[ui.wrapper]">
+    <slot name="leading"></slot>
+    <slot>
+      <span>
+        {{ label }}
+      </span>
+    </slot>
+    <slot name="trailing"></slot>
   </div>
 </template>
